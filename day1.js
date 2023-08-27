651aedc532783eadc76496fb34057e4f937e6a78
wakar-malik <wakarmalik9@gmail.com>
2023-08-27 12:33:06 +0530

    created day-1 file

---
+++ day1.js
@@ -0,0 +1,49 @@
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+console.log("Masai school");
+console.log("A Transformation in Education");
+
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name = "Wakar malik";
+console.log(name);
+
+name = "Shokeen malik";
+console.log(name);
+
+name = "Gulista malik";
+console.log(name);
+
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let userName = "wakar malik";
+let age = 25;
+
+console.log(name);
+console.log(typeof name);
+
+console.log(age);
+console.log(typeof age);
+
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+let user = "Mark";
+let school = "Masai School";
+let marksEnglish = 90;
+let marksMaths = 95;
+let marksScience = 89;
+
+console.log("Name -", name);
+console.log("School Name -", school);
+console.log("English Marks -", marksEnglish);
+console.log("Maths Marks -", marksMaths);
+console.log("Science Marks -", marksScience);
